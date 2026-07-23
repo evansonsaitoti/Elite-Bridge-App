511db16714481d4f5811070e24c51bba01840cd5
Evans Saitoti <122329458+evansonsaitoti@users.noreply.github.com>
2026-07-23 10:17:47 -0400

    Use Hermes-compatible Babel transform profile

--- elitebridge-mobile/babel.config.js
+++ elitebridge-mobile/babel.config.js
@@ -1,6 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          // RN 0.81's Hermes compiler requires private fields to be lowered.
+          unstable_transformProfile: "default",
+        },
+      ],
+    ],
   };
 };
