b62728ecef7a4bc0f586572c53ef364ed7aa451c
Evans Saitoti <122329458+evansonsaitoti@users.noreply.github.com>
2026-07-23 21:25:23 -0400

    Restore NativeWind Babel transform

--- elitebridge-mobile/babel.config.js
+++ elitebridge-mobile/babel.config.js
@@ -1,19 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-<<<<<<< HEAD
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
-=======
     presets: [
       [
         "babel-preset-expo",
         {
+          jsxImportSource: "nativewind",
           // RN 0.81's Hermes compiler requires private fields to be lowered.
           unstable_transformProfile: "default",
         },
       ],
+      "nativewind/babel",
     ],
->>>>>>> a6bb48ca8ed2abbc72d940feb07e16062f4677d4
   };
 };
